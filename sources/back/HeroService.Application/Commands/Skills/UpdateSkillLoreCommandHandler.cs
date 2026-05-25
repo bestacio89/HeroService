884e74f00db69a74ea888a79bbcacfec08db5f52
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-05-25 11:34:06 +0200

    [Feat][Application]: implement skill lore update command handler

---
+++ sources/back/HeroService.Application/Commands/Skills/UpdateSkillLoreCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Franz.Common.Business.Repositories;
+using Franz.Common.Mediator.Handlers;
+using HeroService.Domain.Heroes.Skills;
+
+public sealed class UpdateSkillLoreCommandHandler
+    : ICommandHandler<UpdateSkillLoreCommand>
+{
+  private readonly IEntityRepository<SkillLore, Guid> _repo;
+
+  public UpdateSkillLoreCommandHandler(IEntityRepository<SkillLore, Guid> repo)
+  {
+    _repo = repo;
+  }
+
+  public async Task Handle(UpdateSkillLoreCommand command, CancellationToken ct)
+  {
+    var lore = await _repo.GetByIdAsync(command.SkillLoreId, ct)
+        ?? throw new InvalidOperationException("SkillLore not found.");
+
+    lore.Update(
+        command.Name,
+        command.Description,
+        command.VisualExplanation,
+        command.UpdatedBy
+    );
+
+    await _repo.UpdateAsync(lore, ct);
+  }
+}
