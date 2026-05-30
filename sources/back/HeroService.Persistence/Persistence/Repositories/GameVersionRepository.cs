e3ffd3fc8c5bea6b26f6c4a667950614ebb1e80b
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-05-30 17:42:37 +0200

    [Feat][Persistence]: implement version data access

---
+++ sources/back/HeroService.Persistence/Persistence/Repositories/GameVersionRepository.cs
@@ -0,0 +1,32 @@
+﻿using HeroService.Contracts.Persistence.GameVersions;
+using HeroService.Domain.Heroes.Versioned.GameVersion;
+using Microsoft.EntityFrameworkCore;
+
+namespace HeroService.Persistence.Persistence.Repositories;
+
+public sealed class GameVersionRepository : IGameVersionRepository
+{
+  private readonly ApplicationDbContext _dbContext;
+
+  public GameVersionRepository(ApplicationDbContext dbContext)
+  {
+    _dbContext = dbContext;
+  }
+
+  public Task<GameVersion?> GetByNameAsync(
+      string name,
+      CancellationToken cancellationToken = default)
+  {
+    return _dbContext.Set<GameVersion>()
+        .AsNoTracking()
+        .FirstOrDefaultAsync(x => x.VersionName == name, cancellationToken);
+  }
+
+  public Task<GameVersion?> GetActiveAsync(
+      CancellationToken cancellationToken = default)
+  {
+    return _dbContext.Set<GameVersion>()
+        .AsNoTracking()
+        .FirstOrDefaultAsync(x => x.IsActive, cancellationToken);
+  }
+}
