6b7f4733a4fa4fa7f9aa731dbefb04240f9e94dc
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-06-13 18:31:39 +0200

    [Feat][Application]: implement skill uniqueness validator

---
+++ sources/back/HeroService.Application/Commands/Skills/Services/ISkillUniquenessValidator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeroService.Application.Commands.Skills.Services;
+
+public interface ISkillUniquenessValidator
+{
+  Task EnsureUniqueSkillNameAsync(string name, CancellationToken ct);
+}
---
+++ sources/back/HeroService.Application/Commands/Skills/Services/SkillUniquenessValidator.cs
@@ -0,0 +1,21 @@
+﻿using Franz.Common.Errors;
+using HeroService.Contracts.Persistence.Skills;
+using HeroService.Domain.Heroes.Skills;
+
+namespace HeroService.Application.Commands.Skills.Services;
+
+public sealed class SkillUniquenessValidator : ISkillUniquenessValidator
+{
+  private readonly ISkillRepository _skills;
+
+  public SkillUniquenessValidator(ISkillRepository skills)
+  {
+    _skills = skills;
+  }
+
+  public async Task EnsureUniqueSkillNameAsync(string name, CancellationToken ct)
+  {
+    if (await _skills.ExistsByNameAsync(name, ct))
+      throw new TechnicalException($"Skill '{name}' already exists.");
+  }
+}
