685973def787882bd739d1a5df4a0b08bdbe7277
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-06-13 18:31:38 +0200

    [Feat][Application]: implement hero uniqueness validator

---
+++ sources/back/HeroService.Application/Commands/Heroes/Services/HeroUniquenessValidator.cs
@@ -0,0 +1,29 @@
+﻿using Franz.Common.Business.Repositories;
+using Franz.Common.Errors;
+using HeroService.Contracts.Persistence.Heroes;
+using HeroService.Domain.Heroes.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeroService.Application.Commands.Heroes.Services;
+
+public sealed class HeroUniquenessValidator : IHeroUniquenessValidator
+{
+  private readonly IHeroRepository _heroes;
+
+  public HeroUniquenessValidator(IHeroRepository heroes)
+  {
+    _heroes = heroes;
+  }
+
+  public async Task EnsureUniqueHeroNameAsync(string name, CancellationToken ct)
+  {
+        var exists = await _heroes.ExistsByNameAsync(
+          name,
+          ct);
+
+    if (exists)
+      throw new TechnicalException($"Hero '{name}' already exists.");
+  }
+}
---
+++ sources/back/HeroService.Application/Commands/Heroes/Services/IHeroUniquenessValidator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeroService.Application.Commands.Heroes.Services;
+
+public interface IHeroUniquenessValidator
+{
+  Task EnsureUniqueHeroNameAsync(string name, CancellationToken ct);
+}
