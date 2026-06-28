aa4985946dd98450ee1e6cd30f091d336a46d6d2
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-06-28 23:22:39 +0200

    [Feat][Admin]: implement custom application theme and global styles

---
+++ sources/Front/HeroService.Admin/GameTheme.cs
@@ -0,0 +1,19 @@
+﻿using MudBlazor;
+
+namespace HeroService.Admin.UI;
+
+public static class GameTheme
+{
+  public static MudTheme Create() => new MudTheme()
+  {
+    PaletteDark = new PaletteDark()
+    {
+      Primary = "#D4AF37",
+      Secondary = "#7B3F00",
+      Background = "#0B0F14",
+      Surface = "#121826",
+      AppbarBackground = "#0B0F14",
+      TextPrimary = "#E6E6E6"
+    }
+  };
+}
