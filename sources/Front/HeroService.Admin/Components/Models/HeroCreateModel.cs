ca73f06f2bc9901d7a238fb062566168f56d1071
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-07-03 15:43:28 +0200

    [Feat][Front]: add HeroCreateModel for hero creation forms

---
+++ sources/Front/HeroService.Admin/Components/Models/HeroCreateModel.cs
@@ -0,0 +1,12 @@
+﻿public sealed class HeroCreateModel
+{
+  public string Name { get; set; } = string.Empty;
+
+  public Guid HeroClassId { get; set; }
+
+  public Guid MythologyId { get; set; }
+
+  public Guid OriginCultureId { get; set; }
+
+  public Guid OriginArchetypeId { get; set; }
+}
