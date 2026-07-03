15e956594c12fdb13e01f903a3732f15891fa45b
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-07-03 18:02:12 +0200

    [Feat][Front]: Implement unified skill creation UI

---
+++ sources/Front/HeroService.Admin/Components/Models/CreateSkillModel.cs
@@ -0,0 +1,15 @@
+﻿using HeroService.Contracts.DTOs.Skills;
+using HeroService.Domain.Heroes.Skills;
+
+namespace HeroService.Admin.Components.Models;
+
+public sealed class CreateSkillModel
+{
+  public string Name { get; set; } = string.Empty;
+  public SkillType SkillType { get; set; }
+
+  public SkillLoreDto Lore { get; set; } = new();
+  public SkillBaseStatsDto BaseStats { get; set; } = new();
+
+  public List<SkillEffectDto> Effects { get; set; } = new();
+}
---
+++ sources/Front/HeroService.Admin/Mappers/SkillMappingExtension.cs
@@ -0,0 +1,15 @@
+﻿using HeroService.Admin.Components.Models;
+
+namespace HeroService.Admin.Mappers;
+
+public static class SkillMappingExtensions
+{
+  public static CreateSkillCommand ToCommand(this CreateSkillModel model)
+      => new(
+          model.Name,
+          model.SkillType,
+          model.Lore,
+          model.BaseStats,
+          model.Effects
+      );
+}
