7979f0bcf81a2c5ec9f6fb9bcd73861ba9a2ec73
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-07-03 15:43:24 +0200

    [Feat][ClientHttp]: introduce generic IAdminLifecycleClient for classification management

---
+++ sources/back/HeroService.ClientHttp/Abstractions/IAdminLifecycleClient.cs
@@ -0,0 +1,16 @@
+﻿namespace HeroService.Client.Http.Abstractions;
+
+public interface IAdminLifecycleClient<TDto, TCreate, TRename>
+{
+  Task<IReadOnlyList<TDto>> GetAllAsync(CancellationToken ct = default);
+
+  Task<TDto?> GetByIdAsync(Guid id, CancellationToken ct = default);
+
+  Task<TDto?> GetByNameAsync(string name, CancellationToken ct = default);
+
+  Task<Guid> CreateAsync(TCreate command, CancellationToken ct = default);
+
+  Task RenameAsync(Guid id, TRename command, CancellationToken ct = default);
+
+  Task RetireAsync(Guid id, CancellationToken ct = default);
+}
