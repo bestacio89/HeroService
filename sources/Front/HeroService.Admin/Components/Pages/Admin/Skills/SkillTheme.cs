8634fc0f1e63e37d1a16cb7a3fc9d41f98dd7bd9
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-07-05 19:16:26 +0200

    [Feat][UI]: introduce skilltheme helper for consistent coloring

---
+++ sources/Front/HeroService.Admin/Components/Pages/Admin/Skills/SkillTheme.cs
@@ -0,0 +1,20 @@
+﻿using HeroService.Domain.Heroes.Skills;
+using MudBlazor;
+
+namespace HeroService.Admin.Components.Pages.Admin.Skills;
+
+public static class SkillTheme
+{
+  public static Color GetColor(SkillType type) => type switch
+  {
+    SkillType.Damage => Color.Error,
+    SkillType.Heal => Color.Success,
+    SkillType.Shield => Color.Info,
+    SkillType.Mobility => Color.Secondary,
+    SkillType.Control => Color.Warning,
+    SkillType.Buff => Color.Primary,
+    SkillType.Debuff => Color.Dark,
+    SkillType.Ultimate => Color.Error,
+    _ => Color.Default
+  };
+}
